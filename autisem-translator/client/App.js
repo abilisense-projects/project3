0327eb923d5b8a0dbe84c775839882e25406e533
HadassaAvimor <h0533185978@gmail.com>
2023-12-06 02:39:02 +0200

    change gitignore

--- autisem-translator/client/App.js
+++ autisem-translator/client/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Recorder from './components/recording/recording';
+import RecordAudio from './components/recording/recording';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <RecordAudio></RecordAudio>
       <StatusBar style="auto" />
     </View>
   );
