c9f03e6a018aa2fe5203251331cc89e9d53e6052
HadassaAvimor <h0533185978@gmail.com>
2023-12-21 10:06:02 +0200

    remove unneccesary import

--- autisem-translator/client/App.js
+++ autisem-translator/client/App.js
@@ -9,7 +9,8 @@ import ForgotPassword from "./pages/forgotPassword";
 import CodeFromTheEmail from "./components/login/codeFromTheEmail";
 import NewPassword from "./components/login/newPassword";
 import { translationService } from "./services/translationService";
-import Hamburger from "./components/side_bar/hamburger"; import { Provider, useSelector } from 'react-redux';
+import Hamburger from "./components/side_bar/hamburger";
+ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 export default function App() {
