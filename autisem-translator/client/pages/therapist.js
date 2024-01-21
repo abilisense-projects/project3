a78561a5670ec667dd0b09fcd110803ec9ae13f0
malkyfried <hand1b22@bjcomp.org>
2024-01-21 19:03:16 +0200

    Display pending patients sepratly

--- autisem-translator/client/pages/therapist.js
+++ autisem-translator/client/pages/therapist.js
@@ -12,13 +12,15 @@ import { useNavigation } from '@react-navigation/native';
 
 const TherapistScreen = () => {
   const [patients, setPatients] = useState([]);
+  const [filteredPatients, setFilteredPatients] = useState([]);
   const [isAssociatePatientModalVisible, setAssociatePatientModalVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [bannerMessage, setBannerMessage] = useState(null);
   const [message, setMessage] = useState(null);
   const therapistId = useSelector((state) => state.user.user.userData._id);
   const navigation = useNavigation();
-  
+  const [isDisplayingPending, setIsDisplayingPending] = useState(false);
+
   useEffect(() => {
     fetchData();
   }, [therapistId]);
@@ -29,8 +31,15 @@ const TherapistScreen = () => {
       const patientsData = await therapistService.getTherapistPatients(therapistId);
       if (!patientsData || patientsData.length <= 0) {
         setPatients([]);
+        setFilteredPatients([]);
+      // } else if (patientsData.length === 1 && patientsData[0].status === 'Confirmed') {
+      //   // Automatically navigate to patient details if there's only one patient and status is Confirmed
+      //   navigation.navigate('PatientDetails', { patientId: patientsData[0].patientDetails._id });
       } else {
         setPatients(patientsData);
+        // Display confirmed patients initially
+        const confirmedPatients = patientsData.filter(patient => patient.status === 'Confirmed');
+        setFilteredPatients(confirmedPatients);
       }
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -38,6 +47,17 @@ const TherapistScreen = () => {
     finally {
       setIsLoading(false);
     }
+  };
+
+  const toggleDisplayPending = () => {
+    setIsDisplayingPending(!isDisplayingPending);
+
+    // Update filtered patients based on the new display mode
+    const patientsToDisplay = isDisplayingPending
+      ? patients.filter(patient => patient.status === 'Confirmed')
+      : patients.filter(patient => patient.status === 'Pending');
+
+    setFilteredPatients(patientsToDisplay);
   };
 
   //set opem modal true for entering patient's name
@@ -49,9 +69,9 @@ const TherapistScreen = () => {
     try {
       //first check if it exist in patients
       const isUserNameExists = patients.some(patient => (
-        `${patient.patientDetails.userName}`=== patientUsername
+        `${patient.patientDetails.userName}` === patientUsername
       ));
-  
+
       if (isUserNameExists) {
         setMessage(`This patient already exists on your list.`);
         return;
@@ -74,14 +94,14 @@ const TherapistScreen = () => {
     setAssociatePatientModalVisible(false);
   };
 
-  const handleRemoveSinglePatient =async (patient) => {
+  const handleRemoveSinglePatient = async (patient) => {
     try {
-      const deletion = await therapistService.unAssociatePatient(therapistId,patient.patientDetails._id)
-      if(deletion){
-        console.log("deletion",deletion)
+      const deletion = await therapistService.unAssociatePatient(therapistId, patient.patientDetails._id)
+      if (deletion) {
+        console.log("deletion", deletion)
         // const updatedPatients = patients.filter((p) => p.patientDetails._id !== patient.patientDetails._id);
         // setPatients(updatedPatients);
-        setBannerMessage(`Patient ${patient.patientDetails.firstName} ${patient.patientDetails.lastName} deleted successfully.`);  
+        setBannerMessage(`Patient ${patient.patientDetails.firstName} ${patient.patientDetails.lastName} deleted successfully.`);
       }
     } catch (error) {
       console.error('Error deleting patient:', error);
@@ -116,20 +136,23 @@ const TherapistScreen = () => {
             </View>
           ) : (
             <FlatList
-              data={patients}
+              data={filteredPatients}
               keyExtractor={(item) => item.patientDetails._id}
               renderItem={({ item }) => (
-                <Pressable
-                  onPress={() => navigation.navigate('PatientDetails', { patientId: item.patientDetails._id })}>
-                  <View style={[styles.patientContainer, { backgroundColor: getStatusColor(item.status) }]}>
+             <View style={[styles.patientContainer, { backgroundColor: getStatusColor(item.status) }]}>
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                      <Text style={styles.patientName}>{`${item.patientDetails.firstName} ${item.patientDetails.lastName}`}</Text>
+                      {item.status === 'Confirmed' ? (
+                        <Pressable onPress={() => navigation.navigate('PatientDetails', { patientId: item.patientDetails._id })}>
+                          <Text style={styles.patientName}>{`${item.patientDetails.firstName} ${item.patientDetails.lastName}`}</Text>
+                        </Pressable>
+                      ) : (
+                        <Text style={styles.patientName}>{`${item.patientDetails.firstName} ${item.patientDetails.lastName}`}</Text>
+                      )}
                       <Pressable onPress={() => handleRemoveSinglePatient(item)}>
                         <Ionicons name='trash' style={styles.icon} />
                       </Pressable>
                     </View>
                   </View>
-                </Pressable>
               )}
             />
           )}
@@ -139,6 +162,14 @@ const TherapistScreen = () => {
             title="Add Patient"
             buttonWidth={160}
           />
+           <View style={styles.toggleButtonContainer}>
+            <GenericButton
+              style={styles.toggleButton}
+              onPress={toggleDisplayPending}
+              title={isDisplayingPending ? 'Show Confirmed Patients' : 'Show Pending Patients'}
+              buttonWidth={250}
+            />
+          </View>
           <AssociatePatient
             isVisible={isAssociatePatientModalVisible}
             onConfirm={handleAssociatePatientConfirm}
@@ -222,8 +253,18 @@ const styles = StyleSheet.create({
     height: 100,
   },
   icon: {
-    color:'green',
+    color: 'green',
     fontSize: 24,
+  },
+  toggleButtonContainer: {
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  toggleButton: {
+    backgroundColor: '#4CAF50',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
   },
 });
 
