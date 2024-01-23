8bab7dd175d5566dc42092a4e69434ad53440e38
Michal-Hershowitz <m0527606589@gmail.com>
2024-01-23 23:56:41 +0200

    Change 'Notifications' to 'Manage'

--- autisem-translator/client/App.js
+++ autisem-translator/client/App.js
@@ -133,7 +133,7 @@ const App = () => {
             options={{ title: "" }}
           />
           <Drawer.Screen
-            name="Management"
+            name="Manage"
             component={AssociateTherapist}
             options={{ title: "" }}
           />
